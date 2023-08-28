187f60ad245891a58c50ff052f74b1e8a676a7b3
febriW <febriwanda26@gmail.com>
2023-08-28 13:06:31 +0700

    update test case message module

--- tests/Feature/MessageTest.php
+++ tests/Feature/MessageTest.php
@@ -5,11 +5,13 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use app\Models\Message;
+use App\Models\Message;
+use App\Models\User;
 
 class MessageTest extends TestCase
 {
 
+    use RefreshDatabase;
     private $path = '/api/message';
 
     /**
@@ -17,20 +19,82 @@ class MessageTest extends TestCase
      */
     public function test_get_message(): void
     {
-        Message::factory()->count(32)->make();
+        Message::factory()->count(5)->make();
         $this->get($this->path)
             ->assertOk();
     }
 
     public function test_create_message(){
         $message = Message::factory()->create();
-        $this->postJson($this->path, $person)
+        $this->postJson($this->path, $message->toArray())
             ->assertCreated();
+    }
+
+    public function test_create_message_not_validated()
+    {
+        $message = [
+            'user_id' => '2',
+            'message' => fake()->text(300),
+        ];
+
+        $this->postJson($this->path, $message)
+            ->assertUnprocessable();
     }
     
     public function test_search_message(){
-        $message = Message::factory()->create();
-        $this->postJson($this->path, $person)
+        $user =  User::factory()->create([
+            'name' => 'User Test',
+            'email' => 'usertest@example.com',
+        ]);
+        $message = Message::factory()->create([
+            'user_id' => $user->id,
+            'message' => fake()->text(200),
+        ]);
+
+        $this->postJson($this->path .'/'. 'search',[
+            "params" => $message->message
+        ])->assertOk();
+
+        $this->postJson($this->path .'/'. 'search',[
+            "params" => $user->email
+        ])->assertOk();
+
+        $this->postJson($this->path .'/'. 'search',[
+            "params" => $user->name
+        ])->assertOk();
+    }
+
+    public function test_search_message_not_validated(){
+        $this->postJson($this->path .'/'. 'search')
+            ->assertUnprocessable();
+    }
+
+    public function test_update_message(){
+        $user =  User::factory()->create([
+            'name' => 'User Test',
+            'email' => 'usertest@example.com',
+        ]);
+
+        $message = Message::factory()->create([
+            'user_id' => $user->id,
+            'message' => fake()->text(200),
+        ]);
+        $this->patchJson($this->path .'/'. $message->id, ['message' => $message->message])
             ->assertOk();
+    }
+
+    public function test_message_delete(){
+        $user =  User::factory()->create([
+            'name' => 'User Test',
+            'email' => 'usertest@example.com',
+        ]);
+
+        $message = Message::factory()->create([
+            'user_id' => $user->id,
+            'message' => fake()->text(200),
+        ]);
+
+        $this->delete($this->path .'/'. $message->id)
+            ->assertNoContent($status = 204);
     }
 }
