16380ec3b379b43a9b14b0d877c26dfae0ec80d6
febriW <febriwanda26@gmail.com>
2023-08-28 15:26:01 +0700

    added message request

---
+++ app/Http/Requests/SearchMessageRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class SearchMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'params' => 'required | string | max:200'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => ':attribute tidak boleh kosong',
+            'message.max' => 'Panjang params tidak boleh lebih dari 200 karakter'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
